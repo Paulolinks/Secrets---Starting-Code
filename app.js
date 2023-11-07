b304f14c96cf4b876c419dd41726bb052ec41214
Paulolinks <134472518+Paulolinks@users.noreply.github.com>
2023-11-06 22:37:27 -0500

    Authentication &  Security LEVEL 6 -Using PostgreSQL

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 //Authentication and Security LEVEL 6
+// Import and configure dotenv - arquivo .env onde fica nossas senhas
+import dotenv from "dotenv"; // Store sensitive data in this .env file
+dotenv.config(); // Initialize .env
+
 import express from "express";
 import bodyParser from "body-parser";
 import ejs from "ejs";
@@ -7,6 +11,7 @@ import passport from "passport";
 import { Sequelize, DataTypes } from "sequelize";
 import bcrypt from "bcrypt";
 import { Strategy as LocalStrategy } from "passport-local";
+import { Strategy as GoogleStrategy } from "passport-google-oauth20";
 
 const app = express();
 const port = 3000;
@@ -21,7 +26,9 @@ app.use(
 
 app.use(
   session({
-    secret: "YourSessionSecretHere", // Replace with your secret
+    secret: "YourSessionSecretHere",
+    resave: false, //1
+    saveUninitialized: false, //1
   })
 );
 
@@ -31,11 +38,11 @@ app.use(passport.session());
 
 // Connect to the PostgreSQL database
 // Create a Sequelize instance and define your User model
-const sequelize = new Sequelize("your-table ", "postgres", "your-password ", {
+const sequelize = new Sequelize("User-secret", "postgres", "fl123", {
   host: "localhost",
   dialect: "postgres",
 });
-
+//table collun in banco de dados para encryptar e armazenar
 const User = sequelize.define("users", {
   id: {
     type: DataTypes.INTEGER,
@@ -50,8 +57,26 @@ const User = sequelize.define("users", {
     type: DataTypes.STRING,
     allowNull: false,
   },
+  googleId: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+  secret: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
 });
+// Synchronize the models with the database
+sequelize
+  .sync()
+  .then(() => {
+    console.log("PostgreSQL Database synchronized");
+  })
+  .catch((error) => {
+    console.error("Error synchronizing database:", error);
+  });
 
+// check password in login
 passport.use(
   new LocalStrategy((username, password, done) => {
     User.findOne({ where: { username: username } }).then((user) => {
@@ -86,10 +111,42 @@ passport.deserializeUser((id, done) => {
       done(err, null);
     });
 });
-
+// Login with google account - API -google auth 20
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.CLIENT_ID,
+      clientSecret: process.env.CLIENT_SECRET,
+      callbackURL: "https://localhost:3000/auth/google/secrets",
+      userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo",
+    },
+    function (accessToken, refreshToken, profile, cb) {
+      console.log(profile); // check user
+      //create a user in PostgreSQL data base
+      User.findOrCreate({ googleId: profile.id }, function (err, user) {
+        return cb(err, user);
+      });
+    }
+  )
+);
+//home pag
 app.get("/", function (req, res) {
   res.render("home.ejs");
 });
+// login with google account - authentication with google
+app.get(
+  "/auth/google",
+  passport.authenticate("google", { scope: ["profile"] })
+);
+//send user to page after authentication with google
+app.get(
+  "/auth/google/secrets",
+  passport.authenticate("google", { failureRedirect: "/login" }),
+  function (req, res) {
+    // Successful authentication, redirect secrets.
+    res.redirect("/secrets");
+  }
+);
 
 app.get("/login", function (req, res) {
   res.render("login.ejs");
@@ -99,12 +156,56 @@ app.get("/register", function (req, res) {
   res.render("register.ejs");
 });
 
+// If is authenticated keep in secret pag othewise /loging page
 app.get("/secrets", function (req, res) {
+  User.findAll({
+    where: {
+      secret: {
+        [Sequelize.Op.not]: null, // Use Sequelize's not operator to find non-null secrets
+      },
+    },
+  })
+    .then((foundUsers) => {
+      res.render("secrets", { usersWithSecrets: foundUsers });
+    })
+    .catch((err) => {
+      console.log(err);
+      // Handle the error appropriately
+      res.redirect("/error");
+    });
+});
+
+app.get("/submit", function (req, res) {
   if (req.isAuthenticated()) {
-    res.render("secrets.ejs");
+    res.render("submit");
   } else {
     res.redirect("/login");
   }
+});
+
+// submit your secret find the user Id and insert the secret to database - collunm secret in table
+app.post("/submit", function (req, res) {
+  const submittedSecret = req.body.secret;
+  if (!req.isAuthenticated()) {
+    return res.redirect("/login");
+  }
+
+  const userId = req.user.id; // Get the user's ID from the authenticated session
+
+  User.findByPk(userId)
+    .then((foundUser) => {
+      if (foundUser) {
+        foundUser.secret = submittedSecret;
+        return foundUser.save();
+      }
+    })
+    .then(() => {
+      res.redirect("/secrets");
+    })
+    .catch((err) => {
+      console.error(err);
+      res.redirect("/error"); // Handle the error gracefully
+    });
 });
 
 app.get("/logout", function (req, res) {
@@ -113,19 +214,23 @@ app.get("/logout", function (req, res) {
   });
 });
 
-//post register
+//register
 app.post("/register", function (req, res) {
   const { username, password } = req.body;
+
   // Check if the username and password are provided
   if (!username || !password) {
     return res.redirect("/register");
   }
+
   // Create a new user with Sequelize and hash the password
   bcrypt.hash(password, 10, (err, hash) => {
     if (err) {
       console.log(err);
       return res.redirect("/register");
     }
+
+    // Use the "User" Sequelize model to create a new user
     User.create({
       username: username,
       password: hash,
@@ -141,7 +246,7 @@ app.post("/register", function (req, res) {
         });
       })
       .catch((err) => {
-        console.log(err);
+        console.error("Error inserting user:", err);
         return res.redirect("/register");
       });
   });
--- views/partials/login/logout-code.js
+++ views/partials/login/logout-code.js
@@ -179,107 +179,107 @@ app.listen(port, () => {
 ////////////////////////////////Authentication and Security LEVEL 4///////////////////////////////////////////////////////////
 ////Authentication and Security LEVEL 4
 
-// Import and configure dotenv
-import dotenv from "dotenv"; // Store sensitive data in this .env file
-dotenv.config(); // Initialize .env
+// // Import and configure dotenv
+// import dotenv from "dotenv"; // Store sensitive data in this .env file
+// dotenv.config(); // Initialize .env
 
-import express from "express";
-import bodyParser from "body-parser";
-import ejs from "ejs";
-import pg from "pg";
+// import express from "express";
+// import bodyParser from "body-parser";
+// import ejs from "ejs";
+// import pg from "pg";
 
-//deixando o DB mais seguro
-// Now you can access environment variables
-const { DB_USER, DB_HOST, DB_NAME, DB_PASSWORD, DB_PORT } = process.env;
-const db = new pg.Client({
-  user: DB_USER,
-  host: DB_HOST,
-  database: DB_NAME,
-  password: DB_PASSWORD,
-  port: DB_PORT,
-});
+// //deixando o DB mais seguro
+// // Now you can access environment variables
+// const { DB_USER, DB_HOST, DB_NAME, DB_PASSWORD, DB_PORT } = process.env;
+// const db = new pg.Client({
+//   user: DB_USER,
+//   host: DB_HOST,
+//   database: DB_NAME,
+//   password: DB_PASSWORD,
+//   port: DB_PORT,
+// });
 
-const app = express();
-const port = 3000;
+// const app = express();
+// const port = 3000;
 
-// Connect to the PostgreSQL database
-db.connect((err) => {
-  if (err) {
-    console.error("Error connecting to PostgreSQL:", err);
-  } else {
-    console.log("Connected to PostgreSQL");
-  }
-});
+// // Connect to the PostgreSQL database
+// db.connect((err) => {
+//   if (err) {
+//     console.error("Error connecting to PostgreSQL:", err);
+//   } else {
+//     console.log("Connected to PostgreSQL");
+//   }
+// });
 
-app.use(express.static("public"));
-app.set("view engine", "ejs");
-app.use(
-  bodyParser.urlencoded({
-    extended: true,
-  })
-);
+// app.use(express.static("public"));
+// app.set("view engine", "ejs");
+// app.use(
+//   bodyParser.urlencoded({
+//     extended: true,
+//   })
+// );
 
-app.get("/", function (req, res) {
-  res.render("home.ejs");
-});
+// app.get("/", function (req, res) {
+//   res.render("home.ejs");
+// });
 
-app.get("/login", function (req, res) {
-  res.render("login.ejs");
-});
+// app.get("/login", function (req, res) {
+//   res.render("login.ejs");
+// });
 
-app.get("/register", function (req, res) {
-  res.render("register.ejs");
-});
+// app.get("/register", function (req, res) {
+//   res.render("register.ejs");
+// });
 
-app.get("/secrets", function (req, res) {
-  res.render("secrets.ejs");
-});
+// app.get("/secrets", function (req, res) {
+//   res.render("secrets.ejs");
+// });
 
-// Register user with an MD5-hashed password
-app.post("/register", async function (req, res) {
-  const { username, password } = req.body;
+// // Register user with an MD5-hashed password
+// app.post("/register", async function (req, res) {
+//   const { username, password } = req.body;
 
-  try {
-    const hashedPassword = await bcrypt.hash(password, 10); // Hash the password
-    const query = `
-      INSERT INTO users (username, password)
-      VALUES ($1, $2)
-      RETURNING user_id
-    `;
+//   try {
+//     const hashedPassword = await bcrypt.hash(password, 10); // Hash the password
+//     const query = `
+//       INSERT INTO users (username, password)
+//       VALUES ($1, $2)
+//       RETURNING user_id
+//     `;
 
-    const result = await db.query(query, [username, hashedPassword]); // Store the hashed password
-    console.log("Username: " + username + " Password :" + hashedPassword);
-    res.redirect("/secrets");
-  } catch (error) {
-    console.error("Error inserting data:", error);
-    res.status(500).json({ success: false, error: "Internal Server Error" });
-  }
-});
+//     const result = await db.query(query, [username, hashedPassword]); // Store the hashed password
+//     console.log("Username: " + username + " Password :" + hashedPassword);
+//     res.redirect("/secrets");
+//   } catch (error) {
+//     console.error("Error inserting data:", error);
+//     res.status(500).json({ success: false, error: "Internal Server Error" });
+//   }
+// });
 
-//checking data base and checking password and username
-app.post("/login", async function (req, res) {
-  const { username, password } = req.body;
+// //checking data base and checking password and username
+// app.post("/login", async function (req, res) {
+//   const { username, password } = req.body;
 
-  // Query the database to find a user with the provided username
-  const query = "SELECT * FROM users WHERE username = $1";
-  const result = await db.query(query, [username]);
-  const user = result.rows[0]; // Assuming there is only one user with the provided username
+//   // Query the database to find a user with the provided username
+//   const query = "SELECT * FROM users WHERE username = $1";
+//   const result = await db.query(query, [username]);
+//   const user = result.rows[0]; // Assuming there is only one user with the provided username
 
-  if (user) {
-    const isPasswordValid = await bcrypt.compare(password, user.password); // Verify the password
-    if (isPasswordValid) {
-      // User with matching credentials found, redirect to the secret page
-      res.redirect("/secrets");
-    } else {
-      console.error("Invalid password");
-      res.status(401).json({ success: false, error: "Unauthorized" });
-    }
-  } else {
-    console.error("User not found");
-    res.status(401).json({ success: false, error: "Unauthorized" });
-  }
-});
+//   if (user) {
+//     const isPasswordValid = await bcrypt.compare(password, user.password); // Verify the password
+//     if (isPasswordValid) {
+//       // User with matching credentials found, redirect to the secret page
+//       res.redirect("/secrets");
+//     } else {
+//       console.error("Invalid password");
+//       res.status(401).json({ success: false, error: "Unauthorized" });
+//     }
+//   } else {
+//     console.error("User not found");
+//     res.status(401).json({ success: false, error: "Unauthorized" });
+//   }
+// });
 
-app.listen(port, () => {
-  console.log(`Server running on port ${port}.`);
-});
+// app.listen(port, () => {
+//   console.log(`Server running on port ${port}.`);
+// });
